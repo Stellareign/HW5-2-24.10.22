132e6e188a27774f3f33ece307606ac807c22fdd
Stellareign <tuguran@mail.ru>
2022-10-25 21:48:05 +0300

    Домашка 5, часть 2, задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Домашка 5, часть 2, задание1:");
-        int age = 5;
-        if (age >= 7 && age < 18) {
-            System.out.println("Ребёнок должен ходить в школу.");
+        System.out.println("Домашка 5, часть 2, задание 3:");
+        int ticketsPurchasedSeats = 60;
+        int ticketsPurchasedStanding = 42;
+        int numbеrOfSeats = 60;
+        int numberOfStandingPlcs = 42;
+        int allNumberOfPassengers = 102;
+        int numberFreeSeats = numbеrOfSeats - ticketsPurchasedSeats;
+        int numberFreeStanding = numberOfStandingPlcs - ticketsPurchasedStanding;
+        int totalNumberOfFreePlcs = numberFreeSeats + numberFreeStanding;
+        if (totalNumberOfFreePlcs > 0 && totalNumberOfFreePlcs <= allNumberOfPassengers) {
+            System.out.println("В вагоне есть свободные места. Количество свободных мест " + totalNumberOfFreePlcs);
         }
-        if (age >= 18 && age < 24) {
-            System.out.println("Ребёнок закончил школу и может отправляться в университет.");
+        if (numberFreeSeats > 0 && numberFreeSeats <= 60) {
+            System.out.println("В вагоне есть сидячие места. Количество сидячих мест " + numberFreeSeats);
         }
-        if (age >= 24) {
-            System.out.println("Человек закончил университет и пора искать превую работу."); // пока что работает не совсем правильно :(
-        } else {
-            System.out.println("Ребёнку пока что нужно ходить в десткий сад!");
+        if (numberFreeStanding > 0 && numberFreeStanding <= 42) {
+            System.out.println("В вагоне есть стоячие места. Количество стоячих мест " + numberFreeStanding);
+        }
+        else  {
+            System.out.println("В вагоне нет свободных мест.");
         }
     }
 }
