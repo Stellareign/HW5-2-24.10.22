6d5b7c5e20471098d4acab77e5005ab03b7adf8f
Stellareign <tuguran@mail.ru>
2022-10-25 21:41:56 +0300

    Домашка 5, часть 2, задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Домашка 5, часть 2, задание1:");
-        int age = 16;
-        if (age >=18) {
-            System.out.println("Поздравляю с совершеннолетием!");
+        int age = 5;
+        if (age >= 7 && age < 18) {
+            System.out.println("Ребёнок должен ходить в школу.");
         }
-        else  {
-            System.out.println("Совершеннолетие ещё не наступило, прдётся подожать.");
+        if (age >= 18 && age < 24) {
+            System.out.println("Ребёнок закончил школу и может отправляться в университет.");
+        }
+        if (age >= 24) {
+            System.out.println("Человек закончил университет и пора искать превую работу."); // пока что работает не совсем правильно :(
+        } else {
+            System.out.println("Ребёнку пока что нужно ходить в десткий сад.");
         }
     }
 }
