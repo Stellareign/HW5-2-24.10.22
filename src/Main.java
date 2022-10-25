32d175ec19e03be07e55061613ba0f04f2dd1a80
Stellareign <tuguran@mail.ru>
2022-10-25 21:37:17 +0300

    Домашка 5, часть 2, задание 1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Домашка 5, часть 2, задание1:");
+        int age = 16;
+        if (age >=18) {
+            System.out.println("Поздравляю с совершеннолетием!");
+        }
+        else  {
+            System.out.println("Совершеннолетие ещё не наступило, прдётся подожать.");
+        }
+    }
+}
